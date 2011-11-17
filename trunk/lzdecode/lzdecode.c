1bb132f408b5ef4b27c61c8eb5ab56038f18cfaf
ronieathaydes@gmail.com <ronieathaydes@gmail.com@8c0c9bd9-eaa4-c4cc-3840-aa3f1856e166>
2011-11-17 16:16:32 +0000

    Share project lzdecode.

--- trunk/lzdecode/lzdecode.c
+++ trunk/lzdecode/lzdecode.c
@@ -17,6 +17,73 @@ struct nd {
 	struct nd *next;
 } typedef node;
 
-void decode_file() {
+static void initialize_list(node **list) {
+	*list = NULL;
+}
 
+static void insert_node(node **list, node *n) {
+
+#ifdef DEBUG
+	printf("Lendo elemento %d \"%s\" (%d|\"%c\") ...\n", n->index, n->string, n->prefix, n->new_simbol);
+#endif
+
+	node *p = *list;
+
+    if (p == NULL) {
+    	*list = n;
+    } else {
+    	while (p->next != NULL) {
+    		p = p->next;
+    	}
+
+    	p->next = n;
+    }
+}
+
+static unsigned long read_code(int size) {
+	static unsigned long bit_buffer = 0L;
+	static int bit_count = 0;
+
+	unsigned long return_value = 0;
+
+	while (bit_count <= 24)	{
+		bit_buffer |= (unsigned long)fgetc(codedfile) << (24 - bit_count);
+		bit_count += 8;
+	}
+
+	return_value = bit_buffer >> (32 - size);
+	bit_buffer <<= size;
+	bit_count -= size;
+
+	return return_value;
+}
+
+static void create_dictionary(node **list) {
+	static int index = 1;
+
+	while (!feof(codedfile)) {
+		node *n = calloc(1, sizeof(node));
+		n->index = index;
+
+		n->prefix = 0;
+		if (index == 2) {
+			n->prefix = (int)read_code(1);
+		} else if (index > 2) {
+			int tam_indice = ceil(log2(index - 1));
+			n->prefix = (int)read_code(tam_indice);
+		}
+		n->new_simbol = (char)read_code(sizeof(char) * 8);
+
+		/* TODO inserir string inteira */
+
+		insert_node(list, n);
+		index++;
+	}
+}
+
+void decode_file() {
+	node *list;
+	initialize_list(&list);
+
+	create_dictionary(&list);
 }
