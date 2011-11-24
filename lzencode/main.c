951f007cb357d206c3932d1b32a388353d4c0165
ronieathaydes@gmail.com <ronieathaydes@gmail.com@8c0c9bd9-eaa4-c4cc-3840-aa3f1856e166>
2011-11-24 12:55:24 +0000

    Commit de segurança.

--- lzencode/main.c
+++ lzencode/main.c
@@ -15,8 +15,8 @@ int main(int argc, char *argv[]) {
 		exit (EXIT_FAILURE);
 	}*/
 
-	argv[1] = "/home/padawan/ronie/teste_arquivos/the_prince.txt";
-	argv[2] = "/home/padawan/ronie/codedfile.lz";
+	argv[1] = "/home/ronie/UnB/OA/2-2011/Trabalhos/trab01/teste_arquivos/alice30.txt";
+	argv[2] = "/home/ronie/UnB/OA/2-2011/Trabalhos/trab01/codedfile.lz";
 
 	if ((infile = fopen(argv[1], "r")) == NULL) {
 		printf("O arquivo de entrada nao pode ser aberto.\n");
